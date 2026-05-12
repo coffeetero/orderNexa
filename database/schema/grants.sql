2836ce3585c0221caaf9b806c441efd36dc4f863
coffeetero <moraglen@gmail.com>
2026-05-11 23:00:51 -0400

    grant all on fnd_notes to bps_dev

--- database/schema/grants.sql
+++ database/schema/grants.sql
@@ -4,6 +4,7 @@
 GRANT USAGE ON SCHEMA bps TO anon, authenticated;
 
 GRANT SELECT, INSERT, UPDATE ON TABLE fnd_notes TO authenticated;
+GRANT ALL ON TABLE fnd_notes TO bps_dev;
 
 GRANT SELECT ON TABLE fnd_valuesets TO anon, authenticated;
 GRANT SELECT ON TABLE fnd_valueset_values TO anon, authenticated;
